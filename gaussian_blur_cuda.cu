29b6d80a75d48f54646d63506ed2bb7df4e57ac3
Gabriel Raulet <gabe.h.raulet@gmail.com>
2021-05-26 16:47:52 -0700

    hopefully this works

--- gaussian_blur_cuda.cu
+++ gaussian_blur_cuda.cu
@@ -13,127 +13,96 @@
 
 #define vidx(rr, r, R) (((rr) + (r) < 0) ? 0 : (((rr) + (r) >= (R)) ? (R) - 1 : (rr) + (r)))
 
-//void blur_kernel(float *H, uint8_t *vals, uint8_t *blur, int N, int k, int nrows, int ncols, int ii, int jj)
-//{
-//    int xv, yv, i, j;
-//    float val = 0;
-//    for (i = -k; i <= k; ++i) {
-//        xv = vidx(ii, i, nrows);
-//        for (j = -k; j <= k; ++j) {
-//            yv = vidx(jj, j, ncols);
-//            val += H[(i+k)*N + (j+k)]*vals[xv*ncols + yv];
-//        }
-//    }
-//
-//    blur[ii*ncols + jj] = val;
-//}
-
-__global__ void blur_kernel_cuda(float *H, uint8_t *vals, uint8_t *blur, int N, int k, int nrows, int ncols)
+__global__ void blur_kernel_cuda_pass_one(float *H, float *K, uint8_t *I, int k, int nrows, int ncols)
 {
-    int ii = blockIdx.x * blockDim.x + threadIdx.x;
-    int jj = blockIdx.y * blockDim.y + threadIdx.y;
+    int m = blockIdx.x * blockDim.x + threadIdx.x;
+    int n = blockIdx.y * blockDim.y + threadIdx.y;
 
-    if (ii >= nrows || jj >= ncols) return;
-
-    int xv, yv;
     float val = 0;
-    for (int i = -k; i <= k; ++i) {
-        xv = vidx(ii, i, nrows);
-        for (int j = -k; j <= k; ++j) {
-            yv = vidx(jj, j, ncols);
-            val += H[(i+k)*N + (j+k)]*vals[xv*ncols + yv];
-        }
-    }
-    blur[ii*ncols + jj] = val;
+    for (int j = -k; j <= k; ++j)
+        val += K[j+k]*I[m*ncols + vidx(n, j, ncols)];
+
+    H[m*ncols + n] = val;
+}
+
+__global__ void blur_kernel_cuda_pass_two(float *H, float *K, uint8_t *J, int k, int nrows, int ncols)
+{
+    int m = blockIdx.x * blockDim.x + threadIdx.x;
+    int n = blockIdx.y * blockDim.y + threadIdx.y;
+
+    float val = 0;
+    for (int i = -k; i <= k; ++i)
+        val += K[i+k]*H[vidx(m, i, nrows)*ncols + n];
+
+    J[m*ncols + n] = val;
 }
 
 int main(int argc, char *argv[])
 {
-    float sigma, ss;
+    float sigma, *K_h, *K_d, *H_h, *H_d;
+    uint8_t *I_h, *I_d, *J_h, *J_d;
     int N, k, nrows, ncols, max_pixel;
-    uint8_t *vals_d, *vals_h, *blur_d, *blur_h;
+    FILE *ifp, *ofp;
 
     if (argc != 4) {
         fprintf(stderr, "Usage: ./gaussian_blur_serial <input_pgm> output_pgm> <sigma>\n");
         return 1;
     }
 
+    const char *iname = argv[1];
+    const char *oname = argv[2];
     sigma = atof(argv[3]);
-    ss = sigma * sigma;
+
     N = ceil(6*sigma);
-    if (N % 2 == 0) N++;
+    if (!(N % 2)) N++;
     k = N / 2;
 
     char header[128], n1[32], n2[32];
-    FILE *fp1 = fopen(argv[1], "rb");
-    fgets(header, 128, fp1);
-    char *eol = strchr(header, '\n');
-    *eol = '\0';
 
-    if (strcmp(header, "P5")) {
-        fprintf(stderr, "'%s' must be in P5 format\n", argv[1]);
-        fclose(fp1);
-        return 1;
-    }
+    ifp = fopen(iname, "r");
+    fscanf(ifp, "P5\n%d %d\n%d\n", &ncols, &nrows, &max_pixel);
 
-    fgets(header, 128, fp1);
-    eol = strchr(header, '\n');
-    *eol = '\0';
-    char *hp = header;
-    for (; *hp != ' '; ++hp);
-    *hp = '\0';
+    I_h = (uint8_t *)malloc(nrows*ncols);
+    J_h = (uint8_t *)malloc(nrows*ncols);
+    H_h = (float *)malloc(sizeof(float)*nrows*ncols);
+    K_h = (float *)malloc(sizeof(float)*N);
 
-    strcpy(n1, header);
-    strcpy(n2, hp + 1);
+    fread(I_h, 1, nrows*ncols, ifp);
+    fclose(ifp);
 
-    ncols = atoi(n1);
-    nrows = atoi(n2);
-
-    fgets(header, 128, fp1);
-    eol = strchr(header, '\n');
-    *eol = '\0';
-    max_pixel = atoi(header);
-    vals_h = (uint8_t *) malloc(nrows * ncols);
-    fread(vals_h, 1, nrows * ncols, fp1);
-    fclose(fp1);
-
-    float H[N*N];
-    float *H_d;
     for (int i = 0, x = -k; i < N; ++i, ++x)
-        for (int j = 0, y = -k; j < N; ++j, ++y)
-            H[i*N + j] = exp(-(x*x + y*y)/(2.*ss))/(2.*PI*ss);
+        K_h[i] = exp(-(x*x)/(2.*sigma*sigma))/(sqrt(2.*PI)*sigma);
 
-    blur_h = (uint8_t *) malloc(nrows * ncols);
-    cudaMalloc((void **)&vals_d, nrows * ncols);
-    cudaMalloc((void **)&blur_d, nrows * ncols);
-    cudaMalloc((void **)&H_d, N * N * sizeof(float));
+    cudaMalloc((void **)&I_d, nrows*ncols);
+    cudaMalloc((void **)&J_d, nrows*ncols);
+    cudaMalloc((void **)&H_d, sizeof(float)*nrows*ncols);
+    cudaMalloc((void **)&K_d, sizeof(float)*N);
 
-    cudaMemcpy(vals_d, vals_h, nrows * ncols, cudaMemcpyHostToDevice);
-    cudaMemcpy(H_d, H, N * N * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(I_d, I_h, nrows*ncols, cudaMemcpyHostToDevice);
+    cudaMemcpy(K_d, K_h, sizeof(float), cudaMemcpyHostToDevice);
 
     dim3 block(BLOCK_DIM, BLOCK_DIM);
     dim3 grid(ceil(nrows/(BLOCK_DIM + 0.0)), ceil(ncols/(BLOCK_DIM + 0.0)));
 
-    blur_kernel_cuda<<<grid, block>>>(H_d, vals_d, blur_d, N, k, nrows, ncols);
+    blur_kernel_cuda_pass_one<<<grid, block>>>(H_d, K_d, I_d, k_d, nrows, ncols);
+    blur_kernel_cuda_pass_two<<<grid, block>>>(H_d, K_d, J_d, k_d, nrows, ncols);
 
-    //for (int ii = 0; ii < nrows; ++ii)
-    //    for (int jj = 0; jj < ncols; ++jj)
-    //        blur_kernel(H, vals_h, blur_h, N, k, nrows, ncols, ii, jj);
+    cudaMemcpy(J_h, J_d, nrows*ncols, cudaMemcpyDeviceToHost);
 
-    cudaMemcpy(blur_h, blur_d, nrows * ncols, cudaMemcpyDeviceToHost);
-
-    cudaFree(blur_d);
-    cudaFree(vals_d);
+    cudaFree(I_d);
+    cudaFree(J_d);
     cudaFree(H_d);
+    cudaFree(K_d);
 
-    FILE *fp2 = fopen(argv[2], "wb");
-    fprintf(fp2, "P5\n%d %d\n%d\n", ncols, nrows, max_pixel);
-    fwrite(blur_h, 1, nrows * ncols, fp2);
-    free(blur_h);
-    free(vals_h);
-    fclose(fp2);
+    ofp = fopen(oname, "w");
+    fprintf(ofp, "P5\n%d %d\n%d\n", ncols, nrows, max_pixel);
+    fwrite(J_h, 1, nrows*ncols, ofp);
+    fclose(ofp);
+
+    free(H_h);
+    free(I_h);
+    free(J_h);
+    free(K_h);
     return 0;
 }
-
-
 
